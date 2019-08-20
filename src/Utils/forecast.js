dce076d9469ec6a7c808cb93018809be06ed6cc2
Nikhil Choudhary <nc1336282@gmail.com>
2019-08-20 21:28:14 +0530

    Add additional information for a weather search

--- src/Utils/forecast.js
+++ src/Utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             } else if (body.error) {
                 callback('URL is incorrect!', undefined)
             } else {
-                callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees celcius out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+                callback(undefined, 'It is currently ' + body.currently.temperature + ' degrees celcius out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
             }
         }
     )
